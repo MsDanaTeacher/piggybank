bb78f57d5ed2564aca75f2f41d0e347d552c4807
Dana Lobell <danalobell@gmail.com>
2023-06-07 21:39:09 -0400

    landing page styling begun

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function Landing() {
+  const navigate = useNavigate()
+
   return (
-    <div>Landing</div>
+    <div className='landing-page-main-div'>
+    <nav>
+      <h1>PiggyBank</h1>
+      <button onClick={() => navigate("/login")}>Log In</button>
+    </nav>
+    <div>
+      <div>
+      <h2>Financial literacy is
+important. 
+Spend. Save.
+Learn.</h2>
+<button onClick={() => navigate("/signup")}>Sign Up</button>
+</div>
+    </div>
+    </div>
   )
 }
